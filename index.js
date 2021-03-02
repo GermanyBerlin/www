efa78ef2d5bef518415c5c713d38133f66df7c20
GermanyBerlin <61243904+GermanyBerlin@users.noreply.github.com>
2021-03-02 20:18:27 +0300

    Добавлена модель пользователя и маршруты URL

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors');// для api с другими сайтами
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const path = require('path');
+const config = require('./config/db');
+const account = require('./routes/account');
 
 const app = express();
 
 const port = 3000;
 
+app.use(cors());
+
+app.use(bodyParser.json());
+
+//подключение статической папки
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect(config.db, {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connection.on('connected', () => {
+  console.log('Успешное подключение к Базе Данных!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log('Нет подключения к Базе Данных ' + err);
+});
+
 app.get('/',(req, res) =>{
   res.send('Главная страница сайта!!!');
 });
+
+app.use('/account', account);
 
 app.listen(port, () =>{
   console.log('Сервер запущен по порту: ' + port);
